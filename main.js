2d7040892b38b8641e8049b3d4ce8800ce1a12ed
Juan Briones <jbrionesgmm@gmail.com>
2022-11-09 15:56:42 -0700

    wrote about me bio

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const aboutMeButton = document.querySelector("#aboutMeButton")
+const welcome = document.querySelector("#welcome")
+const myBio = document.querySelector("#myBio")
 
 aboutMeButton.addEventListener('click', function(){
     console.log("clicked")
-    
+    welcome.classList.add("hidden")
+    myBio.classList.remove('hidden')
 })
